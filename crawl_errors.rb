3971f5feb7e8aae6b0290b1a8ab9f7d2bc40ae1e
Simon Ask Ulsnes <simon@ulsnes.dk>
2012-01-26 14:13:53 +0100

    A simple HTTP crawler, that crawls a domain and reports any 500 or 404 errors.

---
+++ crawl_errors.rb
@@ -0,0 +1,16 @@
+require 'uri'
+
+require File.join(File.dirname(__FILE__), 'lib', 'crawl_errors.rb')
+
+uri = URI.parse(ARGV[0]) if ARGV[0]
+invalid_args = !uri.is_a?(URI::HTTP)
+
+if invalid_args || ARGV.include?('--help') || ARGV.include?('-h')
+  $stderr.puts("Error: URL must be a fully qualified HTTP URL (including protocol).") if invalid_args
+  $stderr.puts("Usage: #{$0} <url> [-h|--help] [--report-errors-only]")
+  exit 127
+end
+
+app = CrawlErrors.new(uri)
+app.report_errors_only = ARGV.include?('--report-errors-only')
+app.run
---
+++ lib/crawl_errors.rb
@@ -0,0 +1,72 @@
+require 'rainbow'
+require 'nokogiri'
+require 'uri'
+require 'set'
+require 'net/http'
+
+class CrawlErrors
+  attr_accessor :report_errors_only
+  
+  def initialize(origin_uri)
+    @origin_uri = origin_uri
+    @visited = Set.new
+    @queue = []
+  end
+  
+  def run
+    @queue << @origin_uri
+    until @queue.empty?
+      uri = @queue.shift
+      crawl uri
+    end
+  end
+  
+  def crawl uri
+    return unless @visited.add? uri
+    
+    session = Net::HTTP.new uri.host, uri.port
+    session.start do |http|
+      options = {}
+      options['Accept'] = '*/*'
+      result = http.request_get uri.request_uri, options
+      case result
+      when Net::HTTPSuccess
+        report_success uri
+        body = result.read_body
+        find_and_crawl_links body, uri
+      when Net::HTTPRedirection
+        new_uri = uri.merge(URI.parse(result['location']))
+        report_redirect uri, new_uri
+        @queue << uri unless should_crawl?(new_uri)
+      else
+        report_error uri, result
+      end
+    end
+  end
+  
+  def find_and_crawl_links body, origin
+    page = Nokogiri::HTML(body)
+    page.css('a').each do |a|
+      url = origin.merge(URI.parse(a['href'])) rescue nil
+      if url
+        @queue << url if should_crawl?(url)
+      end
+    end
+  end
+  
+  def should_crawl? uri
+    uri.host == @origin_uri.host && uri.port == @origin_uri.port && !@visited.include?(uri)
+  end
+  
+  def report_error uri, kind
+    $stdout.puts("error".ljust(10).color(:red).bright + uri.to_s + " (#{kind.class.name})".color(:red))
+  end
+  
+  def report_success uri
+    $stdout.puts("ok".ljust(10).color(:green).bright + uri.to_s) unless @report_errors_only
+  end
+  
+  def report_redirect uri, new_uri
+    $stdout.puts("redirect".ljust(10).color(:yellow) + "#{uri} => #{new_uri}") unless @report_errors_only
+  end
+end
